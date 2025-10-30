20ee61f979dc0a85add0d305492953a83200300c
KemberlyOliveira <kemberly.silva@etec.sp.gov.br>
2025-10-30 14:03:56 -0300

    todas as entidades inicializadas

--- ClienteRepository.java
+++
@@ -1,6 +0,0 @@
-package br.com.clininin.clinicar.Repository;
-import org.springframework.data.jpa.repository.JpaRepository;
-import br.com.clininin.clinicar.Entity.Cliente;
-public  interface  ClienteRepository  extends JpaRepository<Cliente, Integer>{
-    
-}
---
+++ clinicar/src/main/java/br/com/clininin/clinicar/Entity/Cliente.java
@@ -0,0 +1,29 @@
+package br.com.clininin.clinicar.Entity;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class Cliente {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+     
+    private Integer idCliente;
+    @Column(nullable = false, length = 40)
+    private String nomeCliente;
+     @Column(nullable = false, length = 40)
+    private String enderecoCliente;
+    @Column(nullable = false, length = 40)
+    private String telefoneCliente;
+}
+
