50e73f0a78cd0b3ee5a376d3f056c0bbd71aa324
4l3xandr3bs <alexandre.soares28@etec.sp.gov.br>
2025-10-30 13:27:56 -0300

    cliente

---
+++ ClienteRepository.java
@@ -0,0 +1,6 @@
+package br.com.clininin.clinicar.Repository;
+import org.springframework.data.jpa.repository.JpaRepository;
+import br.com.clininin.clinicar.Entity.Cliente;
+public  interface  ClienteRepository  extends JpaRepository<Cliente, Integer>{
+    
+}
